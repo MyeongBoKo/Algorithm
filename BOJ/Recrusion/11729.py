6f448b4728c50c5c0e7e67fc94c804a5de9f5697
Tofu <110872008+MyeongBoKo@users.noreply.github.com>
2022-12-23 23:37:55 +0900

    docs: 11729

---
+++ BOJ/Recrusion/11729.py
@@ -0,0 +1,13 @@
+def hanoi(n, start, temp ,end):
+    if n==0:
+        return
+
+    
+    hanoi(n-1, start, end, temp)
+    print(start, end)
+    hanoi(n-1, temp, start, end)
+
+
+n = int(input())
+print(2**n-1)
+hanoi(n,1,2,3)
