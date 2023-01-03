6ad541299c2dc7f370eb6ccffb6de79bd4145be3
Tofu <110872008+MyeongBoKo@users.noreply.github.com>
2023-01-03 20:32:13 +0900

    docs: 9094

---
+++ BOJ/BF/9094.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    count = 0
+    
+    for i in range(1, n):
+        for j in range(i+1, n):
+            if ((i*i+j*j+m)%(i*j)) == 0:
+                count += 1
+    print(count)
+
+    
+        
+        
