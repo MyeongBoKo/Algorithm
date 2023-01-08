7568b5a3c4d2e69b366586880a0827de078bf904
Tofu <110872008+MyeongBoKo@users.noreply.github.com>
2023-01-08 22:26:10 +0900

    docs: 3040

---
+++ BOJ/BF/3040.py
@@ -0,0 +1,11 @@
+num_arr = [int(input()) for _ in range(9)]
+sum_num = sum(num_arr)
+
+for i in range(len(num_arr)):
+    for j in range(i+1, len(num_arr)):
+        if sum_num - num_arr[i] - num_arr[j] == 100:
+            for k in range(len(num_arr)):
+                if k==i or k==j:
+                    pass
+                else:
+                    print(num_arr[k])
