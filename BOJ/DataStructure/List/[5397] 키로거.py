a8438b7289deba8d67eb065b583683aa7be6392e
Tofu <110872008+MyeongBoKo@users.noreply.github.com>
2023-01-20 15:05:34 +0900

    BaekJoon: Solved List

---
+++ BOJ/DataStructure/List/[5397] 키로거.py
@@ -0,0 +1,18 @@
+for _ in range(int(input())):
+    left, right = [], []
+    pwd = input()
+
+    for x in pwd:
+        if x == '>':
+            if right:
+                left.append(right.pop())
+        elif x == '<':
+            if left:
+                right.append(left.pop())
+        elif x == '-':
+            if left:
+                left.pop()
+        else:
+            left.append(x)
+    
+    print(''.join(left+list(reversed(right))))
