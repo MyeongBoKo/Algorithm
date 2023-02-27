4f3fda5734e67c15ee360ad5674c9a16cdad777c
Tofu <110872008+MyeongBoKo@users.noreply.github.com>
2023-02-27 16:39:33 +0900

    BaekJoon: Solved Queue

--- BOJ/DataStructure/Queue&Deque/[1966] 프린터큐.py
+++ BOJ/DataStructure/Queue&Deque/[1966] 프린터큐.py
@@ -1,1 +1,23 @@
+from collections import deque
 
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    q = deque(list(map(int,input().split())))
+    count = 0
+
+    while q:
+        num = max(q)
+        flag = q.popleft()
+        m -= 1
+
+        if num == flag:
+            count += 1
+            if m < 0:
+                print(count)
+                break
+        else:
+            q.append(flag)
+            if m < 0:
+                m = len(q)-1
